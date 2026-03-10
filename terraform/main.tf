88dfb9cc9f3381b62ebda791d827425a8d305a34
Manoj Tatavarthi <sivasaimanojt@gmail.com>
2026-03-11 01:44:58 +0530

    Move images to ECR

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,43 +1,31 @@
-terraform {
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 5.0"
-        }
-    }
+# # Small EC2 Instance
+# data "aws_caller_identity" "current" {}
+
+# resource "aws_instance" "small_ec2" {
+#     ami           = "ami-003ff0e12738bdf26"
+#     instance_type = "t2.micro"
+
+#     tags = {
+#         Name = "small-ec2-instance"
+#     }
+# }
+
+# # Small S3 Bucket
+# resource "aws_s3_bucket" "small_storage" {
+#     bucket = "my-small-bucket-${data.aws_caller_identity.current.account_id}-testing"
+
+#     tags = {
+#         Name = "small-storage-bucket"
+#     }
+# }
+
+
+resource "aws_ecr_repository" "myapp" {
+  name                 = "manoj-ecr-repo"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
 
-provider "aws" {
-    region = "us-east-1"
-}
-
-# Small EC2 Instance
-resource "aws_instance" "small_ec2" {
-    ami           = "ami-0c55b159cbfafe1f0"
-    instance_type = "t2.micro"
-
-    tags = {
-        Name = "small-ec2-instance"
-    }
-}
-
-# Small S3 Bucket
-resource "aws_s3_bucket" "small_storage" {
-    bucket = "my-small-bucket-${data.aws_caller_identity.current.account_id}"
-
-    tags = {
-        Name = "small-storage-bucket"
-    }
-}
-
-# Data source to get AWS Account ID
-data "aws_caller_identity" "current" {}
-
-# Outputs
-output "ec2_instance_id" {
-    value = aws_instance.small_ec2.id
-}
-
-output "s3_bucket_name" {
-    value = aws_s3_bucket.small_storage.id
-}
